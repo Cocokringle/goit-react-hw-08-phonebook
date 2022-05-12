31798cf422748fcaa62c556f2b4b010684ad6ef8
Cocokringle <cocokringle.1@gmail.com>
2022-05-12 18:21:26 +0000

    Deploying to gh-pages from @ Cocokringle/goit-react-hw-08-phonebook@3afccc6f3c3acfee64fab40ecacdd507318ad93f 🚀

---
+++ static/js/280.ae31ed26.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[280],{4543:function(n,e,r){r.d(e,{Z:function(){return i}});var t="Container_container__VVOCq",c=r(184);function i(n){var e=n.children;return(0,c.jsx)("div",{className:t,children:e})}},7280:function(n,e,r){r.r(e),r.d(e,{default:function(){return i}});var t=r(4543),c=r(184);function i(){return(0,c.jsx)(t.Z,{children:(0,c.jsx)("h1",{children:"Oops, this page is not found 404 \ud83d\udc94"})})}}}]);
+//# sourceMappingURL=280.ae31ed26.chunk.js.map
